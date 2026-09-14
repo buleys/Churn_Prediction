3270fc8bb8d429903afbefc0aa43f1d8555d154c
Nombulelo <37207024+buleys@users.noreply.github.com>
2026-09-14 20:19:30 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import joblib
 
 # Load model
-model_bundle = joblib.load("churn_model_pkl")
+model_bundle = joblib.load("churn_model.pkl")
 
 model = nodel_bundle["model"]
 preprocessor = model_bundle["preprocessor"]
